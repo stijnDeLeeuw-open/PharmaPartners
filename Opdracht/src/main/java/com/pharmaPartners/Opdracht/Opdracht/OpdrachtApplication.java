a56963c6dffc4f7bf239e38ea301d2db807c78b7
stijnDeLeeuw-open <76122818+stijnDeLeeuw-open@users.noreply.github.com>
2020-12-20 13:40:21 +0100

    commiting main project version 1
    so many TODO's :(

---
+++ Opdracht/src/main/java/com/pharmaPartners/Opdracht/Opdracht/OpdrachtApplication.java
@@ -0,0 +1,13 @@
+package com.pharmaPartners.Opdracht.Opdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpdrachtApplication.class, args);
+	}
+
+}
---
+++ Opdracht/src/test/java/com/pharmaPartners/Opdracht/Opdracht/OpdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmaPartners.Opdracht.Opdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpdrachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
